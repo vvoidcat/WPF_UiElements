0919127078edc21889362c2f203e286882b48227
vvoidcat <ekaterina.akchurina98@gmail.com>
2023-03-08 00:38:26 +0300

    string spinbox functionality upd

--- Elements/StringSpinBox/StringSpinBox.xaml.cs
+++ Elements/StringSpinBox/StringSpinBox.xaml.cs
@@ -18,6 +18,41 @@ namespace Elements.StringSpinBox {
     /// Interaction logic for StringSpinBox.xaml
     /// </summary>
     public partial class StringSpinBox : UserControl {
+        public static readonly DependencyProperty BoxWidthProperty =
+            DependencyProperty.Register("BoxWidth", typeof(int), typeof(StringSpinBox), new PropertyMetadata(100));
+        public int BoxWidth {
+            get { return (int)GetValue(BoxWidthProperty); }
+            set { SetValue(BoxWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty BoxHeightProperty =
+            DependencyProperty.Register("BoxHeight", typeof(int), typeof(StringSpinBox), new PropertyMetadata(20));
+        public int BoxHeight {
+            get { return (int)GetValue(BoxHeightProperty); }
+            set { SetValue(BoxHeightProperty, value); }
+        }
+
+        public static readonly DependencyProperty boxValueProperty =
+            DependencyProperty.Register("BoxValue", typeof(string), typeof(StringSpinBox), new PropertyMetadata(String.Empty));
+        public string BoxValue {
+            get { return (string)GetValue(boxValueProperty); }
+            set { SetValue(boxValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty decreaseCommandProperty =
+            DependencyProperty.Register("DecreaseCommand", typeof(ICommand), typeof(StringSpinBox), new UIPropertyMetadata(null));
+        public ICommand DecreaseCommand {
+            get { return (ICommand)GetValue(decreaseCommandProperty); }
+            set { SetValue(decreaseCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty increaseCommandProperty =
+            DependencyProperty.Register("IncreaseCommand", typeof(ICommand), typeof(StringSpinBox), new UIPropertyMetadata(null));
+        public ICommand IncreaseCommand {
+            get { return (ICommand)GetValue(increaseCommandProperty); }
+            set { SetValue(increaseCommandProperty, value); }
+        }
+
         public StringSpinBox() {
             InitializeComponent();
         }
--- TestApp/ViewModel/ViewModel.cs
+++ TestApp/ViewModel/ViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -30,7 +31,7 @@ namespace Elements.TestApp {
         }
 
 
-        // IntSpinBox
+        // Int SpinBox
 
         private const int intSboxValueMin = -10;
         private const int intSboxValueMax = 10;
@@ -50,18 +51,71 @@ namespace Elements.TestApp {
         public ICommand intSboxDecreaseCommand { get; private set; }
         public ICommand intSboxIncreaseCommand { get; private set; }
 
-        private void DecreaseSboxValue(object sender) {
+        private void DecreaseIntSboxValue(object sender) {
             intSboxValue -= 1;
         }
 
-        private void IncreaseSboxValue(object sender) {
+        private void IncreaseIntSboxValue(object sender) {
             intSboxValue += 1;
         }
 
 
+        // String SpinBox
+
+        public ObservableCollection<string> strValues = new ObservableCollection<string>() {
+            "aboba", "bebra", "heheheee", "Aboba Flex"
+        };
+
+        private int _chosenValueIndex = 0;
+        private int chosenValueIndex {
+            get { return _chosenValueIndex; }
+            set {
+                if (value >= 0 && value < strValues.Count) {
+                    _chosenValueIndex = value;
+                }
+            }
+        }
+
+        private string _chosenStringValue;
+        public string chosenStringValue {
+            get { 
+                return (strValues is null || strValues.Count == 0) ? "" : _chosenStringValue; 
+            }
+            set {
+                if (strValues is not null && strValues.Count > 0) {
+                    if (_chosenStringValue != value) {
+                        _chosenStringValue = value;
+                        OnPropertyChanged(nameof(chosenStringValue));
+                    }
+                } else {
+                    _chosenStringValue = "";
+                    chosenValueIndex = 0;
+                    OnPropertyChanged(nameof(chosenStringValue));
+                }
+            }
+        }
+
+        public ICommand stringSboxDecreaseCommand { get; private set; }
+        public ICommand stringSboxIncreaseCommand { get; private set; }
+
+        private void DecreaseStringSboxValue(object sender) {
+            chosenValueIndex -= 1;
+            chosenStringValue = strValues[chosenValueIndex];
+        }
+
+        private void IncreaseStringSboxValue(object sender) {
+            chosenValueIndex += 1;
+            chosenStringValue = strValues[chosenValueIndex];
+        }
+
         public ViewModel() {
-            intSboxDecreaseCommand = new RelayCommand<object>(DecreaseSboxValue);
-            intSboxIncreaseCommand = new RelayCommand<object>(IncreaseSboxValue);
+            chosenStringValue = strValues[0];
+
+            intSboxDecreaseCommand = new RelayCommand<object>(DecreaseIntSboxValue);
+            intSboxIncreaseCommand = new RelayCommand<object>(IncreaseIntSboxValue);
+
+            stringSboxDecreaseCommand = new RelayCommand<object>(DecreaseStringSboxValue);
+            stringSboxIncreaseCommand = new RelayCommand<object>(IncreaseStringSboxValue);
         }
     }
 }
